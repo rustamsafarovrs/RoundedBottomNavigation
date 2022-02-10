7ca1d9040ae79f71b1b0c6725070bcb12d7340d2
rustamsafarovrs <rs.programmer@hotmail.com>
2022-02-10 14:04:48 +0500

    shadow and rounded corners

--- app/src/main/java/tj/rs/devteam/app/roundedbottomnavigation/MainActivity.kt
+++ app/src/main/java/tj/rs/devteam/app/roundedbottomnavigation/MainActivity.kt
@@ -1,11 +1,15 @@
 package tj.rs.devteam.app.roundedbottomnavigation
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import tj.rs.devteam.app.roundedbottomnavigation.databinding.ActivityMainBinding
-import kotlin.math.roundToInt
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,7 +24,20 @@ class MainActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         binding.viewModel = viewModel
 
-
+        val sp: ShadowProperty = ShadowProperty()
+            .setShadowColor(
+                ContextCompat.getColor(
+                    this,
+                    R.color.shadow_color
+                )
+            )
+            .setShadowDy(0)
+            .setShadowDx(0)
+            .setShadowRadius(24)
+            .setShadowSide(ShadowProperty.ALL)
+        val sd = ShadowViewDrawable(sp, Color.WHITE, 48f, 48f)
+        ViewCompat.setBackground(binding.materialCardView, sd)
+        binding.materialCardView.setLayerType(View.LAYER_TYPE_HARDWARE, null)
 
     }
 
